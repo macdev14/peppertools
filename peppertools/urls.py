41a75da7e19a62816a4a95b26dda1b48d067198e
lauro <lauropimentel12@gmail.com>
2021-05-31 15:56:54 -0300

    radded admin redirect urls.py

--- peppertools/urls.py
+++ peppertools/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = i18n_patterns(
     # ...
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     # ...
