b42f9291e0254b46f11fbd17304da907f1a90fb9
lauro <lauropimentel12@gmail.com>
2021-06-22 09:37:17 -0300

    o.s cliente

--- pepperadmin/admin.py
+++ pepperadmin/admin.py
@@ -25,7 +25,7 @@ class renderOS(PDFTemplateView, PDFTemplateResponseMixin):
 
 class osModel(DjangoObjectActions, simpleHistory.SimpleHistoryAdmin):
     list_display=('Numero_Os','Cliente','Tipo','Quantidade')
-    search_fields = ('Numero_Os', 'Especificacao', 'Cliente' )
+    search_fields = ('Numero_Os', 'Especificacao', 'Cliente__nome' )
     readonly_fields=('Data',)
     def printos(self, request, obj):
        
