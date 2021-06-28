ed60ed2ce0bcab726045e4365bc51a1751d3a618
lauro <lauropimentel12@gmail.com>
2021-06-28 18:23:44 -0300

    Deploy thumbnail

--- peppertools/settings.py
+++ peppertools/settings.py
@@ -31,9 +31,9 @@ print(BASE_DIR)
 SECRET_KEY = 'm51l@4%c42^q2c2o9l72o@&nc&@ssr93%yrwn5zj5phf2*)520'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-DEBUG_PROPAGATE_EXCEPTIONS = False
-#THUMBNAIL_DEBUG = True
+DEBUG = True
+DEBUG_PROPAGATE_EXCEPTIONS = True
+THUMBNAIL_DEBUG = True
 ALLOWED_HOSTS = ['peppertools.herokuapp.com', 'localhost:8000']
 
 MEDIA_URL = '/media/'
