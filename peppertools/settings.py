b0b24860c528e38753aab20071cc8dfebe627a94
lauro <lauropimentel12@gmail.com>
2022-10-11 08:02:20 -0300

    hosts

--- peppertools/settings.py
+++ peppertools/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 #DEBUG_PROPAGATE_EXCEPTIONS = env('DEBUG')
 THUMBNAIL_DEBUG = env('DEBUG')
-ALLOWED_HOSTS = ['peppertools.herokuapp.com', 'peppertools.lauromtp.com', 'localhost:8000', 'localhost:19006']
+ALLOWED_HOSTS = ['pentercorp.herokuapp.com', 'peppertools.herokuapp.com', 'peppertools.lauromtp.com', 'localhost:8000', 'localhost:19006']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = location('media')
